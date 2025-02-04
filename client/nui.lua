bd66fbdef4feb3859b9e7c753c272c52d43286ef
Artmines <96462463+Artmines@users.noreply.github.com>
2025-02-04 16:28:13 -0700

    Update

--- client/nui.lua
+++ client/nui.lua
@@ -1,246 +1,235 @@
 Citizen.CreateThread(function()
-	while GetIsLoadingScreenActive() do
-		Citizen.Wait(0)
-	end
-	SendNUIMessage({
-		type = "APP_SHOW",
-	})
+    while GetIsLoadingScreenActive() do
+        Citizen.Wait(0)
+    end
+    SendNUIMessage({
+        type = 'APP_SHOW'
+    })
 end)
 
-local loadTo = 0
+
 function loadModel(model)
-	RequestModel(model)
-	loadTo = 0
-	while not HasModelLoaded(model) and loadTo < 500 do
-		loadTo += 1
-		Citizen.Wait(100)
+	if IsModelInCdimage(model) then
+		while not HasModelLoaded(model) do
+			RequestModel(model)
+			Citizen.Wait(5)
+		end
 	end
 end
 
-local previews = {
-	vector4(682.282, 584.414, 129.461, 205.768),
-	vector4(684.203, 585.349, 129.461, 194.392),
-	vector4(680.347, 582.919, 129.461, 239.809),
-	vector4(679.232, 585.493, 129.461, 236.240),
-	vector4(682.157, 587.705, 129.461, 199.840),
-}
-local peds = {}
+function loadAnim(dict)
+    while not HasAnimDictLoaded(dict) do
+        RequestAnimDict(dict)
+        Citizen.Wait(10)
+    end
+end
 
-RegisterNUICallback("GetData", function(data, cb)
-	cb("ok")
+
+local PedAnims = {
+    [1] = {
+        ['Dict'] = "timetable@jimmy@mics3_ig_15@",
+        ['Anim'] = "mics3_15_base_tracy",
+    },
+    [2] = {
+        ['Dict'] = "timetable@ron@ig_3_couch",
+        ['Anim'] = "base",
+    },
+    [3] = {
+        ['Dict'] = "timetable@ron@ig_3_couch",
+        ['Anim'] = "base",
+    },
+    [4] = {
+        ['Dict'] = "mp_sleep",
+        ['Anim'] = "sleep_loop",
+    },
+    [5] = {
+        ['Dict'] = "mp_sleep",
+        ['Anim'] = "sleep_loop",
+    }
+}
+
+local previews = {
+    vector4(-786.12, 339.62, 187.15, 221.59),
+    vector4(-786.14, 337.46, 187.18, 263.59),
+    vector4(-784.72, 339.71, 187.2, 182.73),
+    vector4(-783.28, 339.63, 187.15, 185.47),
+    vector4(682.157, 587.705, 129.461, 199.840),
+}
+
+local peds = {}
+RegisterNUICallback('GetData', function(data, cb)
+    cb("ok")
 
 	while LocalPlayer.state.ID == nil do
 		Citizen.Wait(1)
 	end
 
-	for k, v in ipairs(peds) do
-		DeleteEntity(v)
-	end
+    for k, v in ipairs(peds) do
+        DeleteEntity(v)
+    end
 
-	Callbacks:ServerCallback("Characters:GetServerData", {}, function(serverData)
-		SendNUIMessage({
-			type = "LOADING_SHOW",
-			data = { message = "Getting Character Data" },
-		})
+    Callbacks:ServerCallback('Characters:GetServerData', {}, function(serverData)
+        SendNUIMessage({
+            type = 'LOADING_SHOW',
+            data = { message = 'Getting Character Data' }
+        })
 
-		FadeOutWithTimeout(500)
+        Callbacks:ServerCallback('Characters:GetCharacters', {}, function(characters)
+            SetEntityCoords(PlayerPedId(), -783.09, 334.99, 188.0, 0.0, 0.0, 0.0, false)
 
-		Callbacks:ServerCallback("Characters:GetCharacters", {}, function(characters, characterLimit)
-			local ped = PlayerPedId()
-			SetEntityCoords(ped, 685.865, 576.222, 132.841, 0.0, 0.0, 0.0, false)
-			FreezeEntityPosition(ped, true)
-			SetEntityVisible(ped, false)
-			SetPlayerVisibleLocally(ped, false)
+            local cam2 = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", -783.09, 334.99, 188.0, -10.0, 0.0, 25.81, 60.0, false, 0)
+            SetCamActiveWithInterp(cam2, cam, 1000, true, true)
+            RenderScriptCams(true, false, 1, true, true)
+            TransitionFromBlurred(500)
+            DestroyCam(cam)
+            cam = cam2
 
-			local interior = GetInteriorFromEntity(ped)
-			if interior ~= 0 then
-				local roomHash = GetRoomKeyFromEntity(ped)
-				ForceRoomForEntity(ped, interior, roomHash)
-			end
+            for k, v in ipairs(characters) do
+                if previews[k] then
+                    if v.Preview then
+                        local Anim = PedAnims[k]
+                        loadModel(GetHashKey(v.Preview.model))
+                        local ped = CreatePed(
+                            5,
+                            GetHashKey(v.Preview.model),
+                            previews[k][1],
+                            previews[k][2],
+                            previews[k][3],
+                            previews[k][4],
+                            false,
+                            true
+                        )
+    
+                        while not DoesEntityExist(ped) do
+                            Citizen.Wait(1)
+                        end
+                        loadAnim(Anim['Dict'])
+                        TaskPlayAnim(ped, Anim['Dict'], Anim['Anim'], 2.0, 2.0, -1, 1, 0, false, false, false)
+        
+                        SetEntityCoordsNoOffset(ped, previews[k][1], previews[k][2], previews[k][3])
+                        SetEntityHeading(ped, previews[k][4])
+                        FreezeEntityPosition(ped, true)
+                        Ped:Preview(ped, tonumber(v.Gender), v.Preview, false, v.GangChain)
+    
+                        table.insert(peds, ped)
+                    else
+                        loadModel(tonumber(v.Gender) == 0 and `mp_m_freemode_01` or `mp_f_freemode_01`)
+                        local ped = CreatePed(
+                            5,
+                            tonumber(v.Gender) == 0 and `mp_m_freemode_01` or `mp_f_freemode_01`,
+                            previews[k][1],
+                            previews[k][2],
+                            previews[k][3],
+                            previews[k][4],
+                            false,
+                            true
+                        )
+    
+                        while not DoesEntityExist(ped) do
+                            Citizen.Wait(1)
+                        end
+    
+                        SetEntityCoordsNoOffset(ped, previews[k][1], previews[k][2], previews[k][3])
+                        SetEntityHeading(ped, previews[k][4])
+                        FreezeEntityPosition(ped, true)
+    
+                        table.insert(peds, ped)
+                    end
+                end
+            end
 
-			Citizen.Wait(250)
-
-			local cam2 = CreateCamWithParams(
-				"DEFAULT_SCRIPTED_CAMERA",
-				685.865,
-				576.222,
-				132.841,
-				338.730,
-				0.00,
-				0.00,
-				75.00,
-				false,
-				0
-			)
-			SetCamActiveWithInterp(cam2, cam, 1000, true, true)
-			RenderScriptCams(true, false, 1, true, true)
-
-			TriggerScreenblurFadeOut(500)
-			cam = cam2
-
-			for k, v in ipairs(characters) do
-				if previews[k] then
-					if v.Preview then
-						loadModel(GetHashKey(v.Preview.model))
-						local ped = CreatePed(
-							5,
-							GetHashKey(v.Preview.model),
-							previews[k][1],
-							previews[k][2],
-							previews[k][3],
-							previews[k][4],
-							false,
-							true
-						)
-
-						local t = 0
-						while not DoesEntityExist(ped) and t < 2500 do
-							t += 1
-							Citizen.Wait(1)
-						end
-
-						if DoesEntityExist(ped) then
-							SetEntityCoords(ped, previews[k][1], previews[k][2], previews[k][3], 0.0, 0.0, 0.0, false)
-							FreezeEntityPosition(ped, true)
-							Ped:Preview(ped, tonumber(v.Gender), v.Preview, false, v.GangChain)
-	
-							table.insert(peds, ped)
-						end
-					else
-						loadModel(tonumber(v.Gender) == 0 and `mp_m_freemode_01` or `mp_f_freemode_01`)
-						local ped = CreatePed(
-							5,
-							tonumber(v.Gender) == 0 and `mp_m_freemode_01` or `mp_f_freemode_01`,
-							previews[k][1],
-							previews[k][2],
-							previews[k][3],
-							previews[k][4],
-							false,
-							true
-						)
-
-						local t = 0
-						while not DoesEntityExist(ped) and t < 2500 do
-							t += 1
-							Citizen.Wait(1)
-						end
-
-						if DoesEntityExist(ped) then
-							SetEntityCoords(ped, previews[k][1], previews[k][2], previews[k][3], 0.0, 0.0, 0.0, false)
-							FreezeEntityPosition(ped, true)
-	
-							table.insert(peds, ped)
-						end
-					end
-				end
-			end
-
-			SendNUIMessage({
-				type = "SET_DATA",
-				data = {
-					changelog = serverData.changelog,
-					motd = serverData.motd,
-					characters = characters,
-					characterLimit = characterLimit,
-				},
-			})
-			SendNUIMessage({ type = "LOADING_HIDE" })
-			SendNUIMessage({
-				type = "SET_STATE",
-				data = { state = "STATE_CHARACTERS" },
-			})
-
-			FadeInWithTimeout(500)
-		end)
-	end)
+            SendNUIMessage({
+                type = 'SET_DATA',
+                data = { changelog = serverData.changelog, motd = serverData.motd, characters = characters }
+            })
+            SendNUIMessage({ type = 'LOADING_HIDE' })
+            SendNUIMessage({
+                type = 'SET_STATE',
+                data = { state = 'STATE_CHARACTERS' }
+            })
+        end)
+    end)
 end)
 
-RegisterNUICallback("CreateCharacter", function(data, cb)
-	cb("ok")
-	Callbacks:ServerCallback("Characters:CreateCharacter", data, function(character)
-		if character ~= nil then
-			SendNUIMessage({
-				type = "CREATE_CHARACTER",
-				data = { character = character },
-			})
-		end
+RegisterNUICallback('CreateCharacter', function(data, cb)
+    cb("ok")
+    Callbacks:ServerCallback('Characters:CreateCharacter', data, function(character)
+        if character ~= nil then
+            SendNUIMessage({
+                type = 'CREATE_CHARACTER',
+                data = { character = character }
+            })
+        end
 
-		SendNUIMessage({
-			type = "SET_STATE",
-			data = { state = "STATE_CHARACTERS" },
-		})
-		SendNUIMessage({ type = "LOADING_HIDE" })
-	end)
+        SendNUIMessage({
+            type = 'SET_STATE',
+            data = { state = 'STATE_CHARACTERS' }
+        })
+        SendNUIMessage({ type = 'LOADING_HIDE' })
+    end)
 end)
 
-RegisterNUICallback("DeleteCharacter", function(data, cb)
-	cb("ok")
-	Callbacks:ServerCallback("Characters:DeleteCharacter", data.id, function(status)
-		if status then
-			SendNUIMessage({
-				type = "DELETE_CHARACTER",
-				data = { id = data.id },
-			})
-		end
-		SendNUIMessage({ type = "LOADING_HIDE" })
-	end)
+RegisterNUICallback('DeleteCharacter', function(data, cb)
+    cb("ok")
+    Callbacks:ServerCallback('Characters:DeleteCharacter', data.id, function(status)
+        if status then
+            SendNUIMessage({
+                type = 'DELETE_CHARACTER',
+                data = { id = data.id }
+            })
+        end
+        SendNUIMessage({ type = 'LOADING_HIDE' })
+    end)
 end)
 
-RegisterNUICallback("SelectCharacter", function(data, cb)
-	cb("ok")
-	Callbacks:ServerCallback("Characters:GetSpawnPoints", data.id, function(spawns)
-		if spawns then
-			SendNUIMessage({
-				type = "SET_SPAWNS",
-				data = { spawns = spawns },
-			})
-			SendNUIMessage({
-				type = "SET_STATE",
-				data = { state = "STATE_SPAWN" },
-			})
-		end
+RegisterNUICallback('SelectCharacter', function(data, cb)
+    cb("ok")
+    Callbacks:ServerCallback('Characters:GetSpawnPoints', data.id, function(spawns)
+        if spawns then
+            SendNUIMessage({
+                type = 'SET_SPAWNS',
+                data = { spawns = spawns }
+            })
+            SendNUIMessage({
+                type = 'SET_STATE',
+                data = { state = 'STATE_SPAWN' }
+            })
+        end
 
-		SendNUIMessage({ type = "LOADING_HIDE" })
-	end)
+        SendNUIMessage({ type = 'LOADING_HIDE' })
+    end)
 end)
 
-RegisterNUICallback("PlayCharacter", function(data, cb)
-	cb("ok")
+RegisterNUICallback('PlayCharacter', function(data, cb)
+    cb("ok")
+    Callbacks:ServerCallback('Characters:GetCharacterData', data.character.ID, function(cData)
+        cData.spawn = data.spawn
+        TriggerEvent('Characters:Client:SetData', -1, cData, function()
+            exports['quantum-base']:FetchComponent('Spawn'):SpawnToWorld(cData, function()
+                LocalPlayer.state.canUsePhone = true
+                if data.spawn.event ~= nil then
+                    Callbacks:ServerCallback(data.spawn.event, data.spawn, function()
+                        LocalPlayer.state.Char = cData.ID
+                        TriggerServerEvent('Characters:Server:Spawning')
+                    end)
+                else
+                    LocalPlayer.state.Char = cData.ID
+                    TriggerServerEvent('Characters:Server:Spawning')
+                end
+            end)
+        end)
 
-	FadeOutWithTimeout(500)
-
-	Callbacks:ServerCallback("Characters:GetCharacterData", data.character.ID, function(cData)
-		cData.spawn = data.spawn
-		TriggerEvent("Characters:Client:SetData", -1, cData, function()
-			exports["quantum-base"]:FetchComponent("Spawn"):SpawnToWorld(cData, function()
-				LocalPlayer.state.canUsePhone = true
-				if data.spawn.event ~= nil then
-					Callbacks:ServerCallback(data.spawn.event, data.spawn, function()
-						LocalPlayer.state.Char = cData.ID
-						LocalPlayer.state:set('SID', cData.SID, true)
-						TriggerServerEvent("Characters:Server:Spawning")
-						FadeInWithTimeout(500)
-					end)
-				else
-					LocalPlayer.state:set('SID', cData.SID, true)
-					TriggerServerEvent("Characters:Server:Spawning")
-
-					FadeInWithTimeout(500)
-				end
-			end)
-		end)
-
-		for k, v in ipairs(peds) do
-			DeleteEntity(v)
-		end
-	end)
+        for k, v in ipairs(peds) do
+            DeleteEntity(v)
+        end
+    end)
 end)
 
 RegisterNetEvent("Characters:Client:Spawned", function()
-	TriggerEvent("Characters:Client:Spawn")
-	TriggerServerEvent("Characters:Server:Spawn")
-	SetNuiFocus(false)
-	SendNUIMessage({ type = "APP_HIDE" })
-	SendNUIMessage({ type = "LOADING_HIDE" })
-	LocalPlayer.state.loggedIn = true
+    TriggerEvent('Characters:Client:Spawn')
+    TriggerServerEvent('Characters:Server:Spawn')
+    SetNuiFocus(false)
+    SendNUIMessage({ type = 'APP_HIDE' })
+    SendNUIMessage({ type = 'LOADING_HIDE' })
+    LocalPlayer.state.loggedIn = true
 end)
--- client/spawn.lua
+++ client/spawn.lua
@@ -1,126 +1,92 @@
 local cam = nil
 
-local foTo = 0
-function FadeOutWithTimeout(time, timeOut)
-	DoScreenFadeOut(time or 500)
-	foTo = 0
-	while IsScreenFadingOut() and foTo < (timeOut or 3000) do
-		foTo += 1
-		Citizen.Wait(1)
-	end
-end
-
-local fiTo = 0
-function FadeInWithTimeout(time, timeOut)
-	DoScreenFadeIn(time or 500)
-	fiTo = 0
-	while IsScreenFadingIn() and fiTo < (timeOut or 3000) do
-		fiTo += 1
-		Citizen.Wait(1)
-	end
-end
-
 Spawn = {
-	Choosing = true,
-	InitCamera = function(self)
-		
-		if not IsScreenFadedOut() then
-			FadeOutWithTimeout(500)
-		end
+    Choosing = true,
+    InitCamera = function(self)
+        TransitionToBlurred(500)
+        DoScreenFadeOut(500)
+        cam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", -590.5926, -26.55881, 284.9984, -10.0, 0.0, 25.81, 60.0, false, 0)
+        SetCamActiveWithInterp(cam, true, 900, true, true)
+        RenderScriptCams(true, false, 1, true, true)
+        DisplayRadar(false)
+    end,
+    Init = function(self)
+        local ped = PlayerPedId()
+--        ShutdownLoadingScreenNui()
+        while not HasCollisionLoadedAroundEntity(PlayerPedId()) do -- Added as a 'hotfix' for falling through the ground because collision wasn't loaded yet
+            SetEntityCoords(ped, -783.09, 334.99, 188.0)
+            Citizen.Wait(1)
+        end
+        FreezeEntityPosition(ped, true)
+        SetEntityVisible(ped, false)
+        DoScreenFadeIn(500)
+        Citizen.Wait(500) -- Why the fuck does NUI just not do this without a wait here???
+        SetNuiFocus(true, true)
+        SendNUIMessage({ type = 'APP_SHOW' })
+    end,
+    SpawnToWorld = function(self, data, cb)
+        DoScreenFadeOut(500)
+        while not IsScreenFadedOut() do
+            Citizen.Wait(10)
+        end
+    
+        local player = PlayerPedId()
+        SetTimecycleModifier('default')
+    
+        local model = `mp_f_freemode_01`
+        if tonumber(data.Gender) == 0 then
+            model = `mp_m_freemode_01`
+        end
+    
+        RequestModel(model)
+    
+        while not HasModelLoaded(model) do
+          Citizen.Wait(500)
+        end
+        SetPlayerModel(PlayerId(), model)
+        player = PlayerPedId()
+        SetPedDefaultComponentVariation(player)
+        SetEntityAsMissionEntity(player, true, true)
+        SetModelAsNoLongerNeeded(model)
+    
+        Citizen.Wait(300)
 
-		local ped = PlayerPedId()
-		SetEntityCoords(ped, -972.756, -2701.553, 41.45)
-		FreezeEntityPosition(ped, true)
-		SetEntityVisible(ped, false)
-		SetPlayerVisibleLocally(ped, false)
+        DestroyAllCams(true)
+        RenderScriptCams(false, true, 1, true, true)
+        FreezeEntityPosition(player, false)
+    
+        NetworkSetEntityInvisibleToNetwork(player, false)
+        SetEntityVisible(player, true)
+        FreezeEntityPosition(player, false)
+        cam = nil
+    
+        SetPlayerInvincible(PlayerId(), false)
+        SetCanAttackFriendly(player, true, true)
+        NetworkSetFriendlyFireOption(true)
+        SetEntityCollision(player, true, true)
+    
+        SetEntityMaxHealth(PlayerPedId(), 200)
+        SetEntityHealth(PlayerPedId(), data.HP > 100 and data.HP or 200)
+        DisplayHud(true)
+        
+        if data.action ~= nil then
+            TriggerEvent(data.action, data.data)
+        else
+            SetEntityCoords(player, data.spawn.location.x, data.spawn.location.y, data.spawn.location.z)
+            DoScreenFadeIn(500)
+        end
 
-		TransitionToBlurred(500)
-		cam = CreateCamWithParams(
-			"DEFAULT_SCRIPTED_CAMERA",
-			-90.79,
-			-915.89,
-			236.88,
-			-70.0,
-			0.000,
-			118.395,
-			100.00,
-			false,
-			0
-		)
-		SetCamActiveWithInterp(cam, true, 900, true, true)
-		RenderScriptCams(true, false, 1, true, true)
-		DisplayRadar(false)
-	end,
-	Init = function(self)
-		FadeInWithTimeout(500)
-		Citizen.Wait(500) -- Why the fuck does NUI just not do this without a wait here???
-		SetNuiFocus(true, true)
-		SendNUIMessage({ type = "APP_SHOW" })
-	end,
-	SpawnToWorld = function(self, data, cb)
-		FadeOutWithTimeout(500)
+        SetFocusEntity(PlayerPedId())
+        
+        LocalPlayer.state.ped = player
 
-		local player = PlayerPedId()
-		SetTimecycleModifier("default")
-
-		local model = `mp_f_freemode_01`
-		if tonumber(data.Gender) == 0 then
-			model = `mp_m_freemode_01`
-		end
-
-		RequestModel(model)
-
-		while not HasModelLoaded(model) do
-			Citizen.Wait(500)
-		end
-		SetPlayerModel(PlayerId(), model)
-		player = PlayerPedId()
-		SetPedDefaultComponentVariation(player)
-		SetEntityAsMissionEntity(player, true, true)
-		SetModelAsNoLongerNeeded(model)
-
-		-- Safety check I guess
-		while not IsEntityFocus(player) do
-			ClearFocus()
-			Citizen.Wait(1)
-		end
-
-		Citizen.Wait(300)
-
-		DestroyAllCams(true)
-		RenderScriptCams(false, true, 1, true, true)
-		FreezeEntityPosition(player, false)
-
-		NetworkSetEntityInvisibleToNetwork(player, false)
-		SetEntityVisible(player, true)
-		FreezeEntityPosition(player, false)
-
-		cam = nil
-
-		SetPlayerInvincible(PlayerId(), false)
-		SetCanAttackFriendly(player, true, true)
-		NetworkSetFriendlyFireOption(true)
-
-		SetEntityMaxHealth(player, 200)
-		SetEntityHealth(player, data.HP > 100 and data.HP or 200)
-		DisplayHud(true)
-
-		if data.action ~= nil then
-			TriggerEvent(data.action, data.data)
-		else
-			SetEntityCoords(player, data.spawn.location.x, data.spawn.location.y, data.spawn.location.z)
-			FadeInWithTimeout(500)
-		end
-
-		LocalPlayer.state.ped = player
-
-		SetNuiFocus(false)
-
-		TriggerScreenblurFadeOut(500)
-		cb()
-	end,
+        SetNuiFocus(false)
+    
+        TransitionFromBlurred(500)
+        cb()
+    end
 }
 
-AddEventHandler("Proxy:Shared:RegisterReady", function()
-	exports["quantum-base"]:RegisterComponent("Spawn", Spawn)
+AddEventHandler('Proxy:Shared:RegisterReady', function()
+    exports['quantum-base']:RegisterComponent('Spawn', Spawn)
 end)
